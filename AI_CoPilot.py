61adfb614826431ac68ce8f585f97b6a5a0d5abd
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-08-21 09:40:09 -0400

    Initial commit - AI_CoPilot.py

---
+++ AI_CoPilot.py
@@ -0,0 +1,44 @@
+Sure! Here's a Python script for an AI chatbot that helps the user run the "main.py" program locally on their PC:
+
+```python
+import subprocess
+
+# Function to run the main.py program
+def run_program():
+    try:
+        subprocess.run(["python", "main.py"])
+    except FileNotFoundError:
+        print("Error: Unable to find the main.py file.")
+    except Exception as e:
+        print(f"An error occurred while running the program.\n{e}")
+
+# Function to handle user input and execute commands
+def handle_command(command):
+    if command.lower() == "run":
+        run_program()
+    elif command.lower() == "help":
+        print("Available commands:")
+        print(" - run: Run the main.py program")
+        print(" - help: Show available commands")
+        print(" - exit: Quit the chatbot")
+    elif command.lower() == "exit":
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid command. Type 'help' to see available commands.")
+
+# Main loop
+while True:
+    user_input = input(">>> ")
+    handle_command(user_input)
+```
+
+You can run this script to start the chatbot. The chatbot accepts commands and executes the corresponding actions:
+
+- "run": This command will run the "main.py" program.
+- "help": This command will display the available commands.
+- "exit": This command will quit the chatbot.
+
+Please note that in order to run the "main.py" program successfully, make sure all the required libraries mentioned in the program are installed in your Python environment.
+
+Hope this helps! Let me know if you have any further questions.
